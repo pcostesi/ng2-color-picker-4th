020ff68919de47868b85092907baee20393797e6
Daniel Valenzuela <daniel.valenzuela@4thsource.com>
2017-01-19 17:16:22 -0600

    Testing

--- src/components/color-picker.component.ts
+++ src/components/color-picker.component.ts
@@ -8,11 +8,15 @@ import { IColorPickerConfiguration } from '../interfaces';
     selector: `color-picker[ngModel]`,
     template: `
         <div ngbDropdown class="d-inline-block">
-            <button class="btn btn-outline-primary" id="dropdownMenu1" ngbDropdownToggle>Toggle dropdown</button>
+            <span class="current-color" [style.width]="config.width + 'px'" [style.height]="config.height + 'px'"
+                [style.border-radius]="config.borderRadius + 'px'" [style.background-color]="cd.viewModel" id="dropdownMenu1" ngbDropdownToggle>
+            </span>
             <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
-               <button class="dropdown-item">Action - 1</button>
-                <button class="dropdown-item">Another Action</button>
-                <button class="dropdown-item">Something else is here</button>
+                <ul class="color-picker-dropdown">
+                    <li class="color-picker-color" *ngFor="let color of config.availableColors" [style.width]="config.width + 'px'" [style.height]="config.height + 'px'"
+                        [style.border-radius]="config.borderRadius + 'px'" [style.background-color]="color" (click)="setColor(color)" [title]="color">
+                    </li>
+                </ul>
             </div>
         </div>
     `,
